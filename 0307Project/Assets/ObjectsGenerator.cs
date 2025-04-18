d25b3be416d6d4d130ff6b15bb7469b69829e889
lai12232 <101129234+philia1121@users.noreply.github.com>
2025-04-18 16:25:40 +0800

    4/18 array practice

--- 0307Project/Assets/ObjectsGenerator.cs
+++ 0307Project/Assets/ObjectsGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObjectsGenerator : MonoBehaviour
 {
@@ -13,12 +14,14 @@ public class ObjectsGenerator : MonoBehaviour
     public GameObject prefabD;
     public GameObject prefabE;
     public GameObject prefabF;
+    public GameObject [] prefabs;
     public Transform placement;
     public float drift = 2.2f;
 
     public float timer;
     public float interval;
     public float spawnTime;
+    public int PrefabIndex =0 ;
     void Start()
     {
         for (int num = 0; num < 5; num++){
@@ -35,20 +38,23 @@ public class ObjectsGenerator : MonoBehaviour
             for (int x = -5; x < 5; x++){
                 for (int z = -5; z < 5; z++){
                     drift = Random.Range(1, 6);
-                    Instantiate(prefabA, placement.position + new Vector3 (x*drift, 10, z*drift), Quaternion.identity);
+                    Instantiate(prefabs[0], placement.position + new Vector3 (x*drift, 10, z*drift), Quaternion.identity);
                 }
             }
         }
         if(Input.GetKey(KeyCode.J))
         {
             if(timer>spawnTime){
-                Instantiate(prefabB, placement.position, Quaternion.identity);
+                for (int n=0; n<=prefabs.Length-1;n++){
+                    Instantiate(prefabs[n], placement.position, Quaternion.identity);
+                    Debug.Log("陣列長度"+prefabs.Length);
+                }
                 spawnTime = timer+interval;
             }
         }
         if(Input.GetKeyUp(KeyCode.J))
         {
-            Instantiate(prefabC, placement.position, Quaternion.identity);
+            Instantiate(prefabs[2], placement.position, Quaternion.identity);
         }
 
         if(Input.GetKeyDown(KeyCode.K))
@@ -56,20 +62,25 @@ public class ObjectsGenerator : MonoBehaviour
             for (int x = -5; x < 5; x++){
                 for (int z = -5; z < 5; z++){
                     drift = Random.Range(1, 6);
-                    Instantiate(prefabD, placement.position + new Vector3 (x*drift, 10, z*drift), Quaternion.identity);
+                    Instantiate(prefabs[3], placement.position + new Vector3 (x*drift, 10, z*drift), Quaternion.identity);
                 }
             }
         }
         if(Input.GetKey(KeyCode.K))
         {
             if(timer>spawnTime){
-                Instantiate(prefabE, placement.position, Quaternion.identity);
+                
+                Instantiate(prefabs[PrefabIndex], placement.position, Quaternion.identity);
+                PrefabIndex ++;
+                if(PrefabIndex >prefabs.Length-1){
+                    PrefabIndex=0;
+                }
                 spawnTime = timer+interval;
             }
         }
         if(Input.GetKeyUp(KeyCode.K))
         {
-            Instantiate(prefabF, placement.position, Quaternion.identity);
+            Instantiate(prefabs[5], placement.position, Quaternion.identity);
         }
 
     }
